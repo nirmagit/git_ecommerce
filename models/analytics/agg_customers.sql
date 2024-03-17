8192df9ea043482d430a1b269a9c4f1159c70ff5
nsworld13@outlook.com <nsworld13@outlook.com>
2024-03-17 12:59:10 +0000

    final corrections

--- models/analytics/agg_customers.sql
+++ models/analytics/agg_customers.sql
@@ -12,7 +12,6 @@ customer_orders as (
         c.customer_id,
         o.status as order_status,
         count(o.order_id) as total_orders,
-        count(o.product_id) as total_products,
         sum(o.total_price) as total_sales,
         sum(o.units) as total_units
     from 
@@ -36,7 +35,6 @@ final as (
         c.country,
         coalesce(co.order_status, 'NIL') as order_status,
         coalesce(co.total_orders, 0) as total_orders,
-        coalesce(co.total_products, 0) as total_products,
         coalesce(co.total_sales, 0) as total_sales,
         coalesce(co.total_units, 0) as total_units
 
--- models/marts/fct_orderitems.sql
+++ models/marts/fct_orderitems.sql
@@ -6,17 +6,24 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 distinct_orderitems as (
-    select oi.*
-        , o.units
+    select oi.order_id
+        , oi.customer_id
+        , oi.product_id
+        , oi.status
+        , oi.created_dt
+        , oi.updated_dt
+        , count(product_id) as units
+        , sum(oi.sale_price) as sale_price
         from orderitems oi 
         join orders o
         on oi.order_id = o.order_id 
         and oi.customer_id = o.customer_id
+        group by 1,2,3,4,5,6
 )
 select *
     , (sale_price * units) as total_price
     from 
         distinct_orderitems
         order by 
-            orderitems_id
+            updated_dt
 
